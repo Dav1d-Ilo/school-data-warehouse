c85998a706fe20bdb47e04990e46f32a98e25414
Dav1d-Ilo <iloabuchidavid35@gmail.com>
2026-09-08 11:40:14 +0100

    Create Database.sql

---
+++ Scripts/Database.sql
@@ -0,0 +1,14 @@
+USE master;
+GO
+-- Create the 'SchoolDataWarehouse' Database 
+CREATE DATABASE SchoolDataWarehouse;
+GO
+
+USE SchoolDataWarehouse;
+GO
+-- Craete Schema
+CREATE SCHEMA Bronze;
+GO
+CREATE SCHEMA Silver;
+GO
+CREATE SCHEMA Gold;
